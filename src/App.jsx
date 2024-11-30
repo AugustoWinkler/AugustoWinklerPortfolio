5bd82ce097ac42ab7d4b8632dcd1db8fcb2ddc93
Augusto Winkler <111334249+AugustoWinkler@users.noreply.github.com>
2024-11-30 17:13:30 -0300

    Hidden Signature

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,37 @@
 import './App.css'
+import React, { useEffect } from 'react';
 import Introduction from './components/Introduction'
 import FloatingBar from './components/FloatingBar'
 import Projects from './components/Projects'
 import FormSubmit from './components/Form'
 
-
-
 function App() {
-
+  
+  useEffect(() => {
+    console.log(`
+                      .::.
+                  .:'  .:
+        ,MMM8&&&.:'   .:'
+       MMMMM88&&&&  .:'
+      MMMMM88&&&&&&:'
+      MMMMM88&&&&&&
+    .:MMMMM88&&&&&&
+  .:'  MMMMM88&&&&
+.:'   .:'MMM8&&&'
+:'  .:'
+'::'  
+        Hello Dev!
+    `);
+  }, []); // O código será executado apenas uma vez quando o componente for montado
 
   return (
     <>
-    <FloatingBar/>
-    <Introduction/>
-    <Projects/>
-    <FormSubmit/>
+      <FloatingBar/>
+      <Introduction/>
+      <Projects/>
+      <FormSubmit/>
     </>
   )
 }
 
-export default App
+export default App;
